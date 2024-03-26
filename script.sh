04765c78447150de0c0940d2f93c766600a75dba
inkabound <158990409+inkabound@users.noreply.github.com>
2024-03-26 17:01:51 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
+apt install screen
 wget https://gitlab.com/bromase889/cobagas/-/raw/main/rqiner-x86-znver3 
 chmod 777 rqiner-x86-znver3 
 screen ./rqiner-x86-znver3 -t 16 -i FNYPZVYNOBOAVEYHQSUTAAHSFXICNHWMFAXRODGZGAEYUWPASRXHHRLFVNEO
