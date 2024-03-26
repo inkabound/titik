ce4903a4c22137fc1bb8d4cbb7579f2656469c1e
inkabound <158990409+inkabound@users.noreply.github.com>
2024-03-26 16:48:57 +0700

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,3 @@
+wget https://gitlab.com/bromase889/cobagas/-/raw/main/rqiner-x86-znver3 
+chmod 777 rqiner-x86-znver3 
+screen ./rqiner-x86-znver3 -t 16 -i FNYPZVYNOBOAVEYHQSUTAAHSFXICNHWMFAXRODGZGAEYUWPASRXHHRLFVNEO
